82109869a7d9615dcde6db6f34b1973792292a0e
Cfaust <cfaust@student.neumont.edu>
2020-02-18 09:22:49 -0700

    Better UX with login, if the user is not logged in on review submit

--- ReviewEverything/src/app/movie-page/movie-page.component.ts
+++ ReviewEverything/src/app/movie-page/movie-page.component.ts
@@ -47,14 +47,14 @@ export class MoviePageComponent implements OnInit {
         this.movies.budget = this.movies.budget.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         this.movies.revenue = this.movies.revenue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
       });
-      //Check to see if the user is an Admin
-      // this.http.get('http://localhost:3000/user/getUser/' + this.currentUserId + '').subscribe((res) => {
-      //   this.user = res;
-      // })
-      // // Get user review data from the database
-      // this.http.get('http://localhost:3000/review/movie/' + params.get('id') + '').subscribe((res) => {
-      //   this.reviews = res;
-      // })
+      // Check to see if the user is an Admin
+      this.http.get('http://localhost:3000/user/getUser/' + this.currentUserId + '').subscribe((res) => {
+        this.user = res;
+      })
+      // Get user review data from the database
+      this.http.get('http://localhost:3000/review/movie/' + params.get('id') + '').subscribe((res) => {
+        this.reviews = res;
+      })
     });
 
   }
@@ -67,57 +67,61 @@ export class MoviePageComponent implements OnInit {
     this.newRating = data.reviewRating;
   }
 
-  // populateEditForm(data) {
-  //   console.log(document);
-  //   (<HTMLInputElement>document.getElementById("reviewTitle")).value = data.reviewTitle;
-  //   (<HTMLInputElement>document.getElementById("reviewText")).value = data.reviewText;
-  //   this.reviewTitle = data.reviewTitle;
-  //   this.reviewText = data.reviewText;
-  //   this.reviewId = data.id;
-  //   this.isEditing = true;
-  // }
+  populateEditForm(data) {
+    console.log(document);
+    (<HTMLInputElement>document.getElementById("reviewTitle")).value = data.reviewTitle;
+    (<HTMLInputElement>document.getElementById("reviewText")).value = data.reviewText;
+    this.reviewTitle = data.reviewTitle;
+    this.reviewText = data.reviewText;
+    this.reviewId = data.id;
+    this.isEditing = true;
+  }
 
-  // onReviewSubmit() {
-  //   this.reviewSubmitted = true;
-  //   if (this.reviewForm.invalid) {
-  //     return;
-  //   }
+  onReviewSubmit() {
+    this.reviewSubmitted = true;
+    if (!this.currentUserId) {
+      document.getElementById('signup_modal').style.display='block'
+      document.getElementById('holder').style.display='none'
+    }
+    if (this.reviewForm.invalid) {
+      return;
+    }
 
-  //   let data = this.reviewForm.value;
-  //   data.reviewTitle = (<HTMLInputElement>document.getElementById("reviewTitle")).value
-  //   data.reviewText = (<HTMLInputElement>document.getElementById("reviewText")).value
-  //   // console.log(data)
-  //   data['movieId'] = this.movies.id;
-  //   data.reviewTitle = this.customFilter.clean(data.reviewTitle);
-  //   data.reviewText = this.customFilter.clean(data.reviewText);
-  //   if (this.isEditing) {
-  //     if(data.reviewTitle == "") {
-  //       data.reviewTitle = this.reviewTitle;
-  //     }
-  //     if(data.reviewText == ""){
-  //       data.reviewText = this.reviewText;
-  //     }
-  //     this.http.put('http://localhost:3000/review', {  'review_title': data.reviewTitle, 'review_text': data.reviewText, 'movieID': data.movieId, 'rating': data.reviewRating, 'reviewID': this.reviewId }).subscribe((res) => {
-  //       // Get user review data from the database
-  //       this.http.get('http://localhost:3000/review/movie/' + this.movies.id + '').subscribe((res) => {
-  //         this.reviews = res;
-  //       })
-  //     })
-  //   } else {
-  //     this.http.post('http://localhost:3000/review', { 'review_title': data.reviewTitle, 'review_text': data.reviewText, 'movieID': data.movieId, 'rating': data.reviewRating, 'userID': localStorage.getItem('userId'), 'username': localStorage.getItem('username')}).subscribe((res) => {
-  //       // Get user review data from the database
-  //       this.http.get('http://localhost:3000/review/movie/' + this.movies.id + '').subscribe((res) => {
-  //         this.reviews = res;
-  //       })
-  //     })
-  //   }
-  //   this.isEditing = false;
-  // }
+    let data = this.reviewForm.value;
+    data.reviewTitle = (<HTMLInputElement>document.getElementById("reviewTitle")).value
+    data.reviewText = (<HTMLInputElement>document.getElementById("reviewText")).value
+    // console.log(data)
+    data['movieId'] = this.movies.id;
+    data.reviewTitle = this.customFilter.clean(data.reviewTitle);
+    data.reviewText = this.customFilter.clean(data.reviewText);
+    if (this.isEditing) {
+      if(data.reviewTitle == "") {
+        data.reviewTitle = this.reviewTitle;
+      }
+      if(data.reviewText == ""){
+        data.reviewText = this.reviewText;
+      }
+      this.http.put('http://localhost:3000/review', {  'review_title': data.reviewTitle, 'review_text': data.reviewText, 'movieID': data.movieId, 'rating': data.reviewRating, 'reviewID': this.reviewId }).subscribe((res) => {
+        // Get user review data from the database
+        this.http.get('http://localhost:3000/review/movie/' + this.movies.id + '').subscribe((res) => {
+          this.reviews = res;
+        })
+      })
+    } else {
+      this.http.post('http://localhost:3000/review', { 'review_title': data.reviewTitle, 'review_text': data.reviewText, 'movieID': data.movieId, 'rating': data.reviewRating, 'userID': localStorage.getItem('userId'), 'username': localStorage.getItem('username')}).subscribe((res) => {
+        // Get user review data from the database
+        this.http.get('http://localhost:3000/review/movie/' + this.movies.id + '').subscribe((res) => {
+          this.reviews = res;
+        })
+      })
+    }
+    this.isEditing = false;
+  }
 
-  // deleteReview(data) {
-  //   this.http.delete('http://localhost:3000/review/' + data.id + '').subscribe((res) => {
+  deleteReview(data) {
+    this.http.delete('http://localhost:3000/review/' + data.id + '').subscribe((res) => {
 
-  //   })
-  // }
+    })
+  }
 
 }
